3198bf9717c49b3e00db21455b15050edc8269d5
ProgSomel <somelahmed88@gmail.com>
2024-03-16 01:19:55 +0600

    our recipes left sidebar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,44 @@
-import './App.css'
-import Banner from './components/Bannner/Banner'
-import Navbar from "./components/Navbar/Navbar"
+import { useEffect, useState } from "react";
+import "./App.css";
+import Banner from "./components/Bannner/Banner";
+import Navbar from "./components/Navbar/Navbar";
+import OurRecipe from "./components/OurRecipe/OurRecipe";
 function App() {
+  const [recipes, setRecipes] = useState([]);
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setRecipes(data));
+  }, []);
 
- return (
-  <div className="max-w-screen-lg mx-auto mt-4 font-lexend px-2 md:px-4 lg:px-4">
-    <Navbar></Navbar>
-    <Banner></Banner>
-  </div>
- )
+  return (
+    <div className="max-w-screen-lg mx-auto mt-4 font-lexend px-2 md:px-4 lg:px-4">
+      <Navbar></Navbar>
+      <Banner></Banner>
+
+      <div>
+        <h1 className="text-4xl font-bold text-center mt-16">Our Recipes</h1>
+        <p className="text-center mt-4 font-light">
+          Dive into a world of culinary delights with our extensive recipe
+          collection! Whether you are a seasoned chef or a novice in the kitchen,
+          our carefully curated selection of recipes offers something for
+          everyone.{" "}
+        </p>
+
+        <div className="all-contanier">
+          <div className="lg:w-2/3 grid grid-cols-1 md:grid-cols-2 gap-4 mt-8">
+            {
+              recipes.map(recipe=> <OurRecipe key={recipe.id} recipe={recipe}></OurRecipe>)
+            }
+          </div>
+          <div className="lg:w-1/2">
+            <div className="want-to-cook"></div>
+            <div className="currently-cooking"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/OurRecipe/OurRecipe.jsx
@@ -0,0 +1,50 @@
+import PropTypes from "prop-types";
+import { CiTimer } from "react-icons/ci";
+import { MdOutlineEnergySavingsLeaf } from "react-icons/md";
+const OurRecipe = ({ recipe }) => {
+    const {recipe_name, short_description, ingredients, preparing_time, calories, recipe_image} = recipe;
+
+  return (
+    <div>
+      <div className="card h-[700px]  bg-base-100 shadow-xl">
+       <div className="h-[350px] p-5">
+       <figure>
+          <img className="rounded-lg h-full"
+            src={recipe_image}
+            alt="Shoes"
+          />
+        </figure>
+       </div>
+        <div className="card-body ">
+          <h2 className="card-title">{recipe_name}</h2>
+          <p className="font-light">{short_description}</p>
+          <h1 className="mt-6">Ingredients: { ingredients.length}</h1>
+          <div>
+            {
+                ingredients.map((ingredient, idx)=> <li className="font-light" key={idx}>{ingredient}</li>)
+            }
+          </div>
+          <div className="flex items-center gap-5 mt-2">
+          <div className="flex items-center gap-1">
+          <CiTimer />
+          {preparing_time} <span className="font-light">minutes</span>
+          </div>
+          <div className="flex items-center gap-1">
+          <MdOutlineEnergySavingsLeaf />
+          {calories} <span className="font-light">calories</span>
+          </div>
+          </div>
+          <div className="card-actions mt-3">
+            <button className="btn bg-orange-300 hover:bg-green-300 w-full">Want to Cook</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+OurRecipe.propTypes = {
+  recipe: PropTypes.object,
+};
+
+export default OurRecipe;
